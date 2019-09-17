24cfb5d0fca3f1a7eeb18eeebb1c2a9998c91f38
wamorkart <wamorkar.t@husky.neu.edu>
2019-09-18 00:44:31 +0200

    fix

--- forH4G/reduceWS.py
+++ forH4G/reduceWS.py
@@ -1,11 +1,11 @@
 import ROOT
 from ROOT import *
 
-mass = [10,15,20,25,30,35,40,45,50,55,60]
+mass = [15,20,25,30,35,40,45,50,55,60]
 
 for m in mass:
     print "Looking at mass = ", m
-    inFile = '/afs/cern.ch/work/t/twamorka/ThesisAnalysis/CMSSW_10_5_0/src/flashgg/Signal_Jul15_ver5/signal_'+str(m)+'/signal_m_'+str(m)+'.root'
+    inFile = '/eos/user/t/twamorka/HtoAAto4Gamma_2019/2016Analysis/forCombine/17Sep2019/Signal/'+str(m)+'/add/signal_m_'+str(m)+'.root'
     chain = TChain('h4gCandidateDumper/trees/SUSYGluGluToHToAA_AToGG_M_'+str(m)+'_TuneCUETP8M1_13TeV_pythia8_13TeV_4photons')
     chain.Add(inFile)
     h = ROOT.TH1F("hist","Avg, Diphoton mass;Mass[GeV];# of events",100, 0, 500)
@@ -16,8 +16,9 @@ for m in mass:
     sigma = fitResultPtr.GetParams()[2]
 
     print "mean =",mean, " ", "mean + 2sigma =", mean + 2*sigma, " ", "mean - 2sigma =", mean - 2*sigma
-
-    inFileWS = '/afs/cern.ch/work/t/twamorka/ThesisAnalysis/CMSSW_10_5_0/src/flashgg/Signal_Jul15_ver5/signal_'+str(m)+'/output_SUSYGluGluToHToAA_AToGG_M-'+str(m)+'_TuneCUETP8M1_13TeV_pythia8.root'
+    # print '/eos/user/t/twamorka/HtoAAto4Gamma_2019/2016Analysis/forCombine/17Sep2019/Signal/'+str(m)+'/output_SUSYGluGluToHToAA_AToGG_M-'+str(m)+'_TuneCUETP8M1_13TeV_pythia8.root'
+    inFileWS = '/eos/user/t/twamorka/HtoAAto4Gamma_2019/2016Analysis/forCombine/17Sep2019/Signal/'+str(m)+'/output_SUSYGluGluToHToAA_AToGG_M-'+str(m)+'_TuneCUETP8M1_13TeV_pythia8.root '
+    # inFileWS = '/afs/cern.ch/work/t/twamorka/ThesisAnalysis/CMSSW_10_5_0/src/flashgg/Signal_Jul15_ver5/signal_'+str(m)+'/output_SUSYGluGluToHToAA_AToGG_M-'+str(m)+'_TuneCUETP8M1_13TeV_pythia8.root'
 
     ws_name = 'h4gCandidateDumper/cms_h4g_13TeV'
     dataset_name = 'SUSYGluGluToHToAA_AToGG_M_'+str(m)+'_TuneCUETP8M1_13TeV_pythia8_13TeV_4photons'
@@ -30,11 +31,12 @@ for m in mass:
     temp_dataset_reduced = temp_dataset.reduce('pho1_pt > 30 && pho2_pt > 20 && pho3_pt > 10 && pho4_pt > 10 && abs(pho1_eta) < 2.5 && abs(pho2_eta) < 2.5 && abs(pho3_eta) < 2.5 && abs(pho4_eta) < 2.5 && (abs(pho1_eta) < 1.4442 || abs(pho1_eta) > 1.566) &&  (abs(pho2_eta) < 1.4442 || abs(pho2_eta) > 1.566) && (abs(pho3_eta) < 1.4442 || abs(pho3_eta) > 1.566) && (abs(pho4_eta) < 1.4442 || abs(pho4_eta) > 1.566) && pho1_MVA > -0.9 && pho2_MVA > -0.9 && pho3_MVA > -0.6 && pho4_MVA > -0.6 && tp_mass > 100 && tp_mass < 180 && (avg_dp_mass > '+str(mean-2*sigma) + '&& avg_dp_mass < ' + str(mean+2*sigma)+')')
     print temp_dataset_reduced.numEntries()
 
-    outFileWS = '/eos/cms/store/user/twamorka/HtoAAto4G/Signal/Jul16/w_signal_'+str(m)+'.root'
+    outFileWS = '/eos/user/t/twamorka/HtoAAto4Gamma_2019/2016Analysis/forCombine/17Sep2019/Signal/reducedWS/w_signal_'+str(m)+'.root'
+    # outFileWS = '/eos/cms/store/user/twamorka/HtoAAto4G/Signal/Jul16/w_signal_'+str(m)+'.root'
     output = TFile(outFileWS,'Recreate')
     output.mkdir('tagsDumper')
     output.cd('tagsDumper')
-    ws_new = RooWorkspace('cms_h4g_13TeV')
+    ws_new = RooWorkspace('cms_h4g_13TeV','cms_h4g_13TeV')
     getattr(ws_new,'import')(temp_dataset_reduced,RooCmdArg())
     ws_new.Write()
     output.Close()
