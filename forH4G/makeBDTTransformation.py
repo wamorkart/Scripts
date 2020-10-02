1744d06ca4d5b332c2cf95a564abdac290156f11
wamorkart <wamorkar.t@husky.neu.edu>
2020-10-02 14:10:41 +0200

    bdt transformation script

--- forH4G/makeBDTTransformation.py
+++ forH4G/makeBDTTransformation.py
@@ -12,61 +12,75 @@ if __name__ == '__main__':
 
   parser =  argparse.ArgumentParser(description='BDT transformation')
   parser.add_argument( "-v", "--var",    dest="var", required=True, type=str, help="variable" )
-  parser.add_argument( "-d", "--dir",    dest="dir",  required=True, type=str, help="variable" )
-  parser.add_argument( "-m", "--mass",    dest="mass", required=False, type=str, help="variable" )
-  parser.add_argument( "-t", "--transform",    dest="transform",  required=True, type=str, help="variable" )
+  parser.add_argument( "-d", "--dir",    dest="dir",  required=True, type=str, help="input dir" )
+  parser.add_argument( "-m", "--mass",    dest="mass", required=False, type=str, help="mass" )
+  parser.add_argument( "-y", "--year",    dest="year", required=False, type=str, help="year" )
+  parser.add_argument( "-o", "--opt",    dest="opt", required=False, type=str, help="opt" )
+  parser.add_argument( "-t", "--transform",    dest="transform",  required=True, type=str, help="transformation root file" )
 
   opt = parser.parse_args()
   var = opt.var
   dir = opt.dir
   mass = opt.mass
-  cumulativeGraph_sig = TFile(opt.transform).Get('cumulativeGraph_sig')
+  year = opt.year
+  type = opt.opt
+  cumulativeGraph_sig = TFile(opt.transform).Get('cumulativeGraph')
 
   print "Var    = ", var
   print "Mass   = ", mass
+  print "Transformation Graph = ", opt.transform
+  print "Type of file = ",type
 
-
-  fileName_sig_trans= dir+'signal_m_'+str(mass)+'.root'
-  treeName_sig_trans = 'SUSYGluGluToHToAA_AToGG_M_'+str(mass)+'_TuneCUETP8M1_13TeV_pythia8_13TeV_H4GTag_0'
-  f_sig_trans = TFile(fileName_sig_trans)
+  fileName_sig_trans= ''
+  treename = ''
+  if (type == 'sig'):
+     fileName_sig_trans= dir+'signal_m_'+str(mass)+'_'+str(year)
+     if (year == '2016'):
+         treeName_sig_trans = 'tagsDumper/trees/SUSYGluGluToHToAA_AToGG_M_'+str(mass)+'_TuneCUETP8M1_13TeV_pythia8_13TeV_H4GTag_0'
+         treename = 'SUSYGluGluToHToAA_AToGG_M_'+str(mass)+'_TuneCUETP8M1_13TeV_pythia8_13TeV_H4GTag_0'
+     elif (year == '2017'):
+         treeName_sig_trans = 'tagsDumper/trees/SUSYGluGluToHToAA_AToGG_M_'+str(mass)+'_TuneCP5_13TeV_pythia8_13TeV_H4GTag_0'
+         treename = 'SUSYGluGluToHToAA_AToGG_M_'+str(mass)+'_TuneCP5_13TeV_pythia8_13TeV_H4GTag_0'
+     else:
+         treeName_sig_trans = 'tagsDumper/trees/HAHMHToAA_AToGG_MA_'+str(mass)+'GeV_TuneCP5_PSweights_13TeV_madgraph_pythia8_13TeV_H4GTag_0'
+         treename = 'HAHMHToAA_AToGG_MA_'+str(mass)+'GeV_TuneCP5_PSweights_13TeV_madgraph_pythia8_13TeV_H4GTag_0'
+  elif (type == 'data'):
+      fileName_sig_trans= dir+'data_'+str(year)
+      treeName_sig_trans = 'tagsDumper/trees/Data_13TeV_H4GTag_0'
+      treename = 'Data_13TeV_H4GTag_0'
+  else:
+      # fileName_sig_trans= dir+'data_mix_'+str(year)
+      fileName_sig_trans= dir+'data_mix_'+str(year)+'_highstat_wBDT'
+      treeName_sig_trans = 'tagsDumper/trees/Data_13TeV_H4GTag_0'
+      treename = 'Data_13TeV_H4GTag_0'
+  print "Input tree: ",fileName_sig_trans+'.root'
+  f_sig_trans = TFile(fileName_sig_trans+'.root')
   t_sig_trans = f_sig_trans.Get(treeName_sig_trans)
 
-  # xmin = -1.
-  # xmax = 1.
-  # # precision = 0.000001
-  # precision = 0.00001
 
-  # selection = "(pho1_pt > 30 && pho2_pt > 18 && pho3_pt > 15 && pho4_pt > 15 && abs(pho1_eta) < 2.5 && abs(pho2_eta) < 2.5 && abs(pho3_eta) < 2.5 && abs(pho4_eta) < 2.5 && (abs(pho1_eta) < 1.4442 || abs(pho1_eta) > 1.566) && (abs(pho2_eta) < 1.4442 || abs(pho2_eta) > 1.566) && (abs(pho3_eta) < 1.4442 || abs(pho3_eta) > 1.566) && (abs(pho4_eta) < 1.4442 || abs(pho4_eta) > 1.566) && pho1_electronveto==1 && pho2_electronveto==1 && pho3_electronveto==1 && pho4_electronveto==1 && tp_mass > 110 && tp_mass <180 && pho1_MVA > -999. && pho2_MVA > -999. && pho3_MVA > -999. && pho4_MVA > -999)"
-  # selection = "(pho1_pt > 30 && pho2_pt > 18 && pho3_pt > 15 && pho4_pt > 15 && abs(pho1_eta) < 2.5 && abs(pho2_eta) < 2.5 && abs(pho3_eta) < 2.5 && abs(pho4_eta) < 2.5 && (abs(pho1_eta) < 1.4442 || abs(pho1_eta) > 1.566) && (abs(pho2_eta) < 1.4442 || abs(pho2_eta) > 1.566) && (abs(pho3_eta) < 1.4442 || abs(pho3_eta) > 1.566) && (abs(pho4_eta) < 1.4442 || abs(pho4_eta) > 1.566) && pho1_electronveto==1 && pho2_electronveto==1 && pho3_electronveto==1 && pho4_electronveto==1 && (tp_mass > 110 && tp_mass < 180) && pho1_MVA>-0.9 && pho2_MVA>-0.9 && pho3_MVA>-0.9 && pho4_MVA>-0.9)"
-  # selection = "(pho1_pt > 30 && pho2_pt > 18 && pho3_pt > 15 && pho4_pt > 15 && abs(pho1_eta) < 2.5 && abs(pho2_eta) < 2.5 && abs(pho3_eta) < 2.5 && abs(pho4_eta) < 2.5 && (abs(pho1_eta) < 1.4442 || abs(pho1_eta) > 1.566) && (abs(pho2_eta) < 1.4442 || abs(pho2_eta) > 1.566) && (abs(pho3_eta) < 1.4442 || abs(pho3_eta) > 1.566) && (abs(pho4_eta) < 1.4442 || abs(pho4_eta) > 1.566) && pho1_electronveto==1 && pho2_electronveto==1 && pho3_electronveto==1 && pho4_electronveto==1 && (tp_mass > 110 && tp_mass < 180))"
-
-
-  print "Signal to transform: ", dir+"signal_m_"+str(mass)+"_transformedMVA_finerBinning_27jul2020.root"
-  f_sigTransformed = TFile.Open(dir+"signal_m_"+str(mass)+"_transformedMVA_finerBinning_27jul2020.root","recreate")
-  chain_sig = TChain(t_sig_trans.GetName())
-  chain_sig.Add(fileName_sig_trans)
+  # print "Signal to transform: ", dir+"signal_m_"+str(mass)+"_"+str(year)+"_transformedMVA.root"
+  # f_sigTransformed = TFile.Open(dir+"signal_m_"+str(mass)+"_"+str(year)+"_transformedMVA.root","recreate")
+  f_sigTransformed = TFile.Open(fileName_sig_trans+"_transformedMVA.root","recreate")
+  chain_sig = TChain('tagsDumper/trees/'+str(t_sig_trans.GetName()))
+  chain_sig.Add(fileName_sig_trans+'.root')
+  # print chain_sig.GetEntries()
   copyTree = chain_sig.CopyTree("")
-  copyTree.SetName(treeName_sig_trans)
-  copyTree.SetTitle(treeName_sig_trans)
+  copyTree.SetName(treename)
+  copyTree.SetTitle(treename)
+
+  # print copyTree.GetEntries()
 
   transfMVA = array( 'f', [ 0. ] )
   transfBranch = copyTree.Branch("bdtTransformed",transfMVA,"bdtTransformed/F");
 
   for i,event in enumerate(copyTree):
      if i>copyTree.GetEntries():break
-
+     # print event.bdt
      mva = event.bdt
+     # print cumulativeGraph_sig
      transfMVA[0] = cumulativeGraph_sig.Eval(mva)
-     print i, "  " , transfMVA[0]
+     # print i, "  " , transfMVA[0]
      transfBranch.Fill()
 
   f_sigTransformed.Write()
   f_sigTransformed.Close()
-
-  # f_sigTransformed = TFile(dir+'signal_m_'+str(mass)+'_transformedMVA_differentselection_27jul2020.root')
-  # t_sigTransformed = f_sigTransformed.Get(treeName_sig_trans)
-  # histoMVAtrans_sig = TH1F("histoMVAtrans_sig","histoMVAtrans_sig",100,0.,xmax)
-  # histoMVAtrans_sig.Sumw2()
-  # t_sigTransformed.Draw("bdtTransformed >> histoMVAtrans_sig","weight*36*"+selection,"goff")
-  #
-  # print "Total signal integral: ", histoMVAtrans_sig.Integral()
