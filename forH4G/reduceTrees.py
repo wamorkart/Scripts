7c7589b1e6af2b92e160b3e6e23c02ac6e55890f
wamorkart <wamorkar.t@husky.neu.edu>
2020-11-24 09:35:51 +0100

    script to skim data ntuples

--- forH4G/reduceTrees.py
+++ forH4G/reduceTrees.py
@@ -1,13 +1,14 @@
 import ROOT
 import argparse
 
-parser =  argparse.ArgumentParser(description='Apply Pairing BDT')
-parser.add_argument('-i', '--i', dest='i', required=True, type=str)
-parser.add_argument('-m', '--m', dest='m', required=True, type=str)
+parser =  argparse.ArgumentParser(description='Skim data ntuples')
+parser.add_argument('-y', '--y', dest='y', required=True, type=str)
+parser.add_argument('-e', '--e', dest='e', required=True, type=str)
 opt = parser.parse_args()
 
-f1 = ROOT.TFile(opt.i+'.root')
-ntuple = f1.Get('SUSYGluGluToHToAA_AToGG_M_'+opt.m+'_TuneCUETP8M1_13TeV_pythia8_13TeV_4photons')
-f2 = ROOT.TFile(opt.i+'_skim.root','recreate')
+infile = '/eos/user/t/twamorka/h4g_fullRun2/withSystematics/'+str(opt.y)+'/Data_NoPreselectionsApplied/hadd/data_'+str(opt.e)
+f1 = ROOT.TFile(infile+'.root')
+ntuple = f1.Get('tagsDumper/trees/Data_13TeV_H4GTag_0')
+f2 = ROOT.TFile(infile+'_skim.root','recreate')
 small = ntuple.CopyTree('pho1_pt > 30 && pho2_pt > 18 && pho3_pt > 15 && pho4_pt > 15 && abs(pho1_eta) < 2.5 && abs(pho2_eta) < 2.5 && abs(pho3_eta) < 2.5 && abs(pho4_eta) < 2.5 && (abs(pho1_eta) < 1.4442 || abs(pho1_eta) > 1.566) && (abs(pho2_eta) < 1.4442 || abs(pho2_eta) > 1.566) && (abs(pho3_eta) < 1.4442 || abs(pho3_eta) > 1.566) && (abs(pho4_eta) < 1.4442 || abs(pho4_eta) > 1.566) && pho1_electronveto==1 && pho2_electronveto==1 && pho3_electronveto==1 && pho4_electronveto==1  && tp_mass > 110 && tp_mass < 180')
 small.Write()
--- forH4G/runreduceTrees.sh
+++ forH4G/runreduceTrees.sh
@@ -1,7 +1,40 @@
 #!/bin/bash
-#for i in 60 55 50 45 40 35 30 25 20 15;
-for i in 60;
-do
-  echo ${i}
-  python reduceTrees.py --i /eos/user/t/twamorka/16April2020_Ntuples_OldPairing/Signal/signal_m_${i} 
-done
+python reduceTrees.py -y 2016 -e B
+python reduceTrees.py -y 2016 -e C
+python reduceTrees.py -y 2016 -e D
+python reduceTrees.py -y 2016 -e E
+python reduceTrees.py -y 2016 -e F
+python reduceTrees.py -y 2016 -e G
+python reduceTrees.py -y 2016 -e H
+
+python reduceTrees.py -y 2017 -e B
+python reduceTrees.py -y 2017 -e C
+python reduceTrees.py -y 2017 -e D
+python reduceTrees.py -y 2017 -e E
+python reduceTrees.py -y 2017 -e F
+
+python reduceTrees.py -y 2018 -e A
+python reduceTrees.py -y 2018 -e B
+python reduceTrees.py -y 2018 -e C
+python reduceTrees.py -y 2018 -e D
+#year='2016 2017 2018'
+#for y in ${year};
+#do
+#  if [["$y" -eq  2016]];then
+#  era='B C D E F G H'
+
+#  fi
+#  if [$y==2017];then
+#  era='B C D E F'
+#  fi
+#  if [$y==2018];then
+#  era='A B C D'
+#  fi
+#  echo $era
+#  for e in era;
+#  do
+#    echo 
+     #python reduceTrees.py -y ${y} -e ${e} #  done
+#  done
+
+#done
